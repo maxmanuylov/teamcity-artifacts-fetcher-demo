84c5034af64d5de112c79ca1b66b85966c207649
Maxim Manuylov <maxim.manuylov@gmail.com>
2013-12-17 12:02:15 +0100

    first commit

---
+++ src/com/mycompany/MyClass.java
@@ -0,0 +1,11 @@
+package com.mycompany;
+
+/**
+ * @author Maxim.Manuylov
+ *         Date: 17.12.13
+ */
+public class MyClass {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
